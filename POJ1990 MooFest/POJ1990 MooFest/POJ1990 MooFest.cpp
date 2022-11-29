3206f36da81bb39f26d5245dc5bf847a0ee20c0f
liuxynb <2096321485@qq.com>
2022-11-29 21:12:23 +0800

    2022/11/29 submit

---
+++ POJ1990 MooFest/POJ1990 MooFest/POJ1990 MooFest.cpp
@@ -0,0 +1,72 @@
+﻿//POJ1990 MooFest
+//2022.11.29
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAXN 20005
+#define ll long long
+
+ll c[2][MAXN]; //两个树状数组,c[0]存点数，c[1]存距离。
+ll all_dis;
+ll ans;
+
+struct COW
+{
+	int v, x;//v是牛的权值，x是牛的位置
+}cows[MAXN];
+//树状数组，
+ll lowbit(ll x);
+ll get_sum(ll x, bool op);
+void modify(ll x, bool op);
+int comp(const COW& a, const COW& b);
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		scanf("%d%d", &cows[i].v, &cows[i].x);
+	sort(cows + 1, cows + 1 + n, comp);
+	for (int i = 1; i <= n; i++)
+	{
+		ll cnt = get_sum(cows[i].x, 0);
+		ll now_dis = get_sum(cows[i].x, 1);
+		ll left = cnt * cows[i].x - now_dis;
+		ll right = all_dis -now_dis- (i - cnt - 1) * cows[i].x;
+		ans += (left + right) * cows[i].v;
+		all_dis += cows[i].x;
+		modify(cows[i].x, 0);
+		modify(cows[i].x, 1);
+	}
+	printf("%lld", ans);
+	return 0;
+}
+ll lowbit(ll x)
+{
+	return x & (-x);
+}
+ll get_sum(ll x, bool op)
+{
+	ll sum = 0;
+	for (ll i = x; i; i -= lowbit(i))
+	{
+		sum += c[op][i];
+	}
+	return sum;
+}
+void modify(ll x, bool op)
+{
+	for (ll i = x; i <= MAXN; i += lowbit(i))
+	{
+		if (op)
+			c[op][i] += x;
+		else
+			c[op][i]++;
+	}
+}
+int comp(const COW& a, const COW& b)
+{
+	return a.v < b.v;
+}
--- POJ3321 Apple Tree （树状数组）/POJ3321 Apple Tree （树状数组）/POJ3321 Apple Tree （树状数组）.cpp
+++ POJ3321 Apple Tree （树状数组）/POJ3321 Apple Tree （树状数组）/POJ3321 Apple Tree （树状数组）.cpp
@@ -1,20 +1,85 @@
-﻿// POJ3321 Apple Tree （树状数组）.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿//树状数组（BIT）
+//2022.11.23
+#include<stdio.h>
+#define MAXN 1000001
+int n, x, y, cnt; //cnt用于计数。
+int tre[MAXN], start[MAXN], out[MAXN];
+int vec[MAXN], next[MAXN], head[MAXN];
+int exist[MAXN];     //储存是否有苹果。
 
-#include <iostream>
-
-int main()
+void Add_edge(int x, int y)//加边
 {
-    std::cout << "Hello World!\n";
+	vec[++cnt] = x; next[cnt] = head[y]; head[y] = cnt;
+	vec[++cnt] = y; next[cnt] = head[x]; head[x] = cnt;
 }
+int Lowbit(int x)// BIT中重要的概念
+{
+	return x & (-x);
+}
+void Modify(int x, int rank)//更新BIT数组状态
+{
+	while (x<=n)
+	{
+		tre[x] = tre[x] + rank;
+		x = x + Lowbit(x); //自底向上
+	}
+}
+int Get_sum(int t)
+{
+	int sum = 0;
+	while (t > 0)
+	{
+		sum += tre[t];
+		t -= Lowbit(t);
+	}
+	return sum;
+}
+int Get_branch(int x, int y)
+{
+	return Get_sum(y) - Get_sum(x - 1);
+}
+void Dfn(int now, int last) //类似先序遍历
+{
+	start[now] = ++cnt; //cnt是时间戳，dfn数组里存的是dfn序
+	for (int i = head[now]; i != 0; i = next[i])
+	{
+		if (vec[i] != last)Dfn(vec[i], now);
+	}
+	out[now] = cnt; //out数组存的是start的子树在dfn序中对应的序列的末尾的下标！，以求树枝上的果数。
+}
+int main(void)
+{
+	scanf("%d", &n);
+	for (int i = 1; i < n; i++)
+	{
+		scanf("%d%d", &x, &y);
+		Add_edge(x, y);
+	}
+	cnt = 0;//重置计数器
+	Dfn(1, -1);
+	for (int i = 1; i <= n; i++)
+	{
+		Modify(i, 1);
+		exist[i] = 1;
+	}
+	char str[3];
+	int m;
+	scanf("%d", &m);
+	for (int i = 1; i <= m; i++)
+	{
+		scanf("%s%d", str, &x);
+		if (str[0] == 'Q')
+			printf("%d\n", Get_branch(start[x], out[x]));
+		else if (exist[start[x]])
+		{
+			Modify(start[x], -1);
+			exist[start[x]] = 0;
+		}
+		else
+		{
+			Modify(start[x], 1);
+			exist[start[x]] = 1;
+		}
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	}
+}
