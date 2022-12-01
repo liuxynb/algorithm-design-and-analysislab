1d08f2f754796bb8575e84e04d2be12df8b27e7b
liuxynb <2096321485@qq.com>
2022-12-01 22:01:48 +0800

    2022/12/1

---
+++ POJ1986 Distance Queries(最近公共祖先lca,离线Tarjan算法，最短路)/POJ1986 Distance Queries(最近公共祖先lca,离线Tarjan算法，最短路)/POJ1986 Distance Queries(最近公共祖先lca,离线Tarjan算法，最短路).cpp
@@ -0,0 +1,218 @@
+﻿////2022.11.30
+//#include<stdio.h>
+//#include<string.h>
+//#define ll long long
+//#define MAXN 1000001
+//int pre[MAXN], head[MAXN], head2[MAXN];
+//int tot, tot2;
+//int dis[MAXN], ans[MAXN];
+//bool vis[MAXN], vis2[MAXN];
+//int n;
+//struct Edge
+//{
+//	int v, w;//w是权值
+//	int next;
+//}edge[MAXN];
+//struct Query
+//{
+//	int v, next, id;
+//}query[MAXN];
+//
+//int find_f(int x)
+//{
+//	return x == pre[x] ? x : pre[x] = find_f(pre[x]);
+//}
+//void add_edge(int u, int v, int w)
+//{
+//	edge[tot].v = v;
+//	edge[tot].w = w;
+//	edge[tot].next = head[u];
+//	head[u] = tot++;
+//}
+//void add_query(int u, int v, int id)
+//{
+//	query[tot2].v = v;
+//	query[tot2].id = id;
+//	query[tot2].next = head2[u];
+//	head2[u] = tot2++;
+//}
+//void dfs(int u, int len)//求各个节点在树中的深度。
+//{
+//	vis2[u] = 1;
+//	dis[u] = len;
+//	for (int i = head[u]; i != -1; i = edge[i].next)
+//	{
+//		int v = edge[i].v;
+//		int w = edge[i].w;
+//		if (!vis[v])
+//			dfs(v, len + w);
+//	}
+//}
+//void lca(int u, int f)//f即父亲节点
+//{
+//	for (int i = head[u]; i != -1; i = edge[i].next)
+//	{
+//		int v = edge[i].v;
+//		if (v == f)continue;
+//		lca(v, u);
+//		pre[v] = u;
+//	}
+//	vis[u] = 1;
+//	for (int i = head2[u]; i != -1; i = query[i].next)
+//	{
+//		int v = query[i].v;
+//		if (vis[v])
+//		{
+//			int id = query[i].id;
+//			ans[id] = dis[u] + dis[v] - 2 * dis[find_f(v)];
+//		}
+//	}
+//}
+//
+//int main(void)
+//{
+//	int m, qu;
+//	while (~scanf("%d%d",&n,&m))
+//	{
+//		for (int i = 1; i <= n; i++)
+//			pre[i] = i;
+//		memset(head, -1, sizeof(head));
+//		memset(head2, -1, sizeof(head2));
+//		memset(vis, 0, sizeof(vis));
+//		memset(vis2, 0, sizeof(vis2));
+//		memset(dis, 0, sizeof(dis));
+//		tot = tot2 = 0;/*初始化链式前项星*/
+//		int u, v, w;
+//		char str[3];
+//		for (int i = 1; i <= m; i++)
+//		{
+//			scanf("%d%d%d%s", &u, &v, &w, str);
+//			add_edge(u, v, w);
+//			add_edge(v, u, w);
+//		}
+//		scanf("%d", &qu);
+//		for (int i = 1; i <= qu; i++)
+//		{
+//			scanf("%d%d", &u, &v);
+//			add_query(u, v, i);
+//			add_query(v, u, i);
+//		}
+//		dfs(1, 0);
+//		lca(1, 1);
+//		for (int i = 1; i <= qu; i++)
+//			printf("%d", ans[i]);
+//		return 0;
+//
+//	}
+//}
+/*2022.11.30*/
+#include<stdio.h>
+#include<string.h>
+typedef long long ll;
+const int MAXN = 400001;
+int pre[MAXN], head[MAXN], head2[MAXN];
+int tot, tot2;
+bool vis[MAXN], vis2[MAXN];
+int n;
+int dis[MAXN], ans[MAXN];
+struct Edge
+{
+    int v, w, next;/*w是权值*/
+} edge[MAXN];
+struct Query
+{
+    int v, next, id;
+} query[MAXN];
+/*加边*/
+void add_edge(int u, int v, int w);
+void add_query(int u, int v, int id);
+/*找爸爸*/
+int find_f(int x);
+/*求各个节点在树中的深度。*/
+void dfs(int u, int len);
+/*f即父亲节点*/
+void lca(int u, int f);
+int main(void)
+{
+    int m, q;    
+    while (~scanf("%d%d", &n, &m))
+    {
+        for (int i = 1; i <= n; i++)
+		    pre[i] = i;
+		memset(head, -1, sizeof(head));
+		memset(head2, -1, sizeof(head2));
+		memset(vis, 0, sizeof(vis));
+		memset(vis2, 0, sizeof(vis2));
+		memset(dis, 0, sizeof(dis));
+		tot = tot2 = 0;/*初始化链式前项星*/
+        int u, v, w;
+        char c[5];
+        for (int i = 1; i <= m; i++)
+        {           
+            scanf("%d%d%d%s", &u, &v, &w, c);
+            add_edge(u, v, w);
+            add_edge(v, u, w);
+        }
+        scanf("%d", &q);
+        for (int i = 1; i <= q; i++)
+        {
+            scanf("%d%d", &u, &v);
+            add_query(u, v, i);
+            add_query(v, u, i);
+        }
+        dfs(1, 0);lca(1, 1);
+        for (int i = 1; i <= q; i++)
+            printf("%d\n", ans[i]);
+    }
+    return 0;
+}
+void add_edge(int u, int v, int w)
+{
+    edge[tot].v = v;
+    edge[tot].w = w;
+    edge[tot].next = head[u];
+    head[u] = tot++;
+}
+void add_query(int u, int v, int id)
+{
+    query[tot2].id = id;
+    query[tot2].v = v;
+    query[tot2].next = head2[u];
+    head2[u] = tot2++;
+}
+int find_f(int x)
+{
+    return x == pre[x] ? x : pre[x] = find_f(pre[x]);
+}
+void dfs(int u, int len)/*求各个节点在树中的深度。*/
+{
+    vis2[u] = 1;
+    dis[u] = len;
+    for (int i = head[u]; i != -1; i = edge[i].next)
+    {
+        int v = edge[i].v, w = edge[i].w;
+        if (!vis2[v])
+            dfs(v, len + w);
+    }
+}
+void lca(int u, int f)/*f即父亲节点*/
+{
+    for (int i = head[u]; i != -1; i = edge[i].next)
+    {
+        int v = edge[i].v;
+        if (v == f) continue;
+        lca(v, u);
+        pre[v] = u;
+    }
+    vis[u] = 1;
+    for (int i = head2[u]; ~i; i = query[i].next)
+    {
+        int v = query[i].v;
+        if (vis[v])
+        {
+            int id = query[i].id;
+            ans[id] = dis[u] + dis[v] - 2 * dis[find_f(v)];
+        }
+    }
+}
+
---
+++ poj1470(LCA模板题)/poj1470(LCA模板题)/poj1470(LCA模板题).cpp
@@ -0,0 +1,127 @@
+﻿//2022.11.30
+#include<stdio.h>
+#include<cmath>
+#define MAXN 901
+int first[MAXN<<1], node[MAXN<<1], depth[MAXN<<1], dp[MAXN << 1][25];
+int ans[MAXN], vis[MAXN];
+struct EDGE
+{
+	int v;
+	int next;
+}edge[MAXN];
+int head[MAXN], cnt, tot, in[MAXN];
+//建树
+void add_vec(int u, int v);
+//利用dfs构造欧拉序列
+void dfs(int u, int d);
+//预处理st表
+void rmq_init(int n);
+//利用rmq算法
+int rmq(int x, int y);
+//找最近公共祖先
+int lca(int a, int b);
+
+int main(void)
+{
+	int n, q;
+	while (scanf("%d",&n)!=EOF)
+	{
+		for (int i = 0; i < MAXN; i++)
+		{
+			ans[i] = vis[i] = in[i] = 0;
+			head[i] = -1;
+		}
+		tot = cnt = 0;
+		int u, v, m;
+		for (int i = 0; i < n; i++) //输入
+		{
+			scanf("%d:(%d)", &u, &m);
+			for (int j = 0; j < m; j++)
+			{
+				scanf("%d", &v);
+				in[v]++;
+				add_vec(u, v);
+			}
+		}
+		for (int i = 1; i <= n; i++)
+			if (!in[i])//从根开始
+				dfs(i, 1);//从深度为1开始
+		rmq_init(tot);//tot即欧拉序列长度
+		scanf("%d", &q);
+		while (q--)
+		{
+			while (getchar() != '(');
+            scanf("%d%d", &u, &v);
+            int h = lca(u, v);
+            ans[h] += 1;
+            while (getchar() != ')');
+		}
+		for (int i = 1; i <= n; i++)
+			if (ans[i]) printf("%d:%d\n", i, ans[i]);
+	}
+}
+
+void add_vec(int u, int v)
+{
+	edge[cnt].v = v;
+	edge[cnt].next = head[u];
+	head[u] = cnt++;
+}
+void dfs(int u, int d)
+{
+	node[++tot] = u;//将u加入欧拉序中
+	depth[tot] = d;
+	vis[u] = 1;
+	first[u] = tot;//记录u第一次出现的位置
+	for (int i = head[u]; i != -1; i = edge[i].next)
+	{
+		if (vis[edge[i].v] == 0)
+		{
+			dfs(edge[i].v, d + 1); //继续dfs，深度加1
+			node[++tot] = u;//回溯时也需要加入欧拉序中
+			depth[tot] = d;
+		}
+	}
+}
+void rmq_init(int n)//预处理st表
+{
+	int k = log2(n);
+	for (int i = 1; i <= n; i++)
+		dp[i][0] = i;
+	for (int j = 1; j <= k; j++)
+	{
+		for (int i = 1; i + (1 << j) - 1 <= n; i++)
+		{
+			int a = dp[i][j - 1];
+			int b = dp[i + (1 << j - 1)][j - 1];//dp[i][j]即长度为2^(j-1)，起始位置为为i的段的最小深度对应的下标。
+			if (depth[a] < depth[b])
+				dp[i][j] = a;
+			else
+				dp[i][j] = b;
+		}
+	}
+}
+int rmq(int x, int y)
+{
+	int k = log2(y - x + 1);
+	int a = dp[x][k];
+	int b = dp[y - (1 << k) + 1][k];
+	if (depth[a] < depth[b]) return a;
+	return b;
+}
+int lca(int a, int b)
+{
+	int x = first[a];
+	int y = first[b];
+	int k;
+	if (x <= y)
+	{
+		k = rmq(x, y);
+		return node[k];
+	}
+	else
+	{
+		k = rmq(y, x);
+		return node[k];
+	}
+}
